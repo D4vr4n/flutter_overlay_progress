ca2d37efd03cdb53d2a80980157b59b1fbae7a1e
D4vr4n <davron2001davron@gmail.com>
2022-02-26 13:03:24 +0600

    initial commit

---
+++ lib/overlay_progress.dart
@@ -0,0 +1,51 @@
+library overlay_progress;
+
+import 'package:flutter/material.dart';
+
+class OverlayProgress extends StatelessWidget {
+  final bool inAsyncCall;
+  final double opacity;
+  final Color color;
+  final Widget progressIndicator;
+  final Offset? offset;
+  final bool dismissible;
+  final Widget child;
+
+  const OverlayProgress({
+    Key? key,
+    required this.inAsyncCall,
+    this.opacity = 0.3,
+    this.color = Colors.grey,
+    this.progressIndicator = const CircularProgressIndicator(),
+    this.offset,
+    this.dismissible = false,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (!inAsyncCall) return child;
+
+    Widget layOutProgressIndicator;
+    if (offset == null) {
+      layOutProgressIndicator = Center(child: progressIndicator);
+    } else {
+      layOutProgressIndicator = Positioned(
+        child: progressIndicator,
+        left: offset!.dx,
+        top: offset!.dy,
+      );
+    }
+
+    return Stack(
+      children: [
+        child,
+        Opacity(
+          child: ModalBarrier(dismissible: dismissible, color: color),
+          opacity: opacity,
+        ),
+        layOutProgressIndicator,
+      ],
+    );
+  }
+}
